7fae3ba2d86a121a03ab6dcbb946b97ed83331ec
shen <kk1036588@163.com>
2019-12-09 18:29:54 +0800

    项目提交

---
+++ src/main/java/com/woniu/pojo/User.java
@@ -0,0 +1,27 @@
+package com.woniu.pojo;
+
+public class User {
+	
+	private Integer userId;
+	private String userName;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public User(Integer userId, String userName) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+	}
+	
+	
+
+}
