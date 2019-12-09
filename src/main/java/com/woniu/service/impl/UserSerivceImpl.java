b9c00d7250c2b7f1dc57ac6bd4a4add065129242
xin <24537qq.com>
2019-12-09 21:54:24 +0800

    "service实现类"

--- src/main/java/com/woniu/service/impl/UserSerivceImpl.java
+++ src/main/java/com/woniu/service/impl/UserSerivceImpl.java
@@ -2,44 +2,49 @@ package com.woniu.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.woniu.dao.UserDao;
 import com.woniu.pojo.User;
 import com.woniu.service.UserService;
 
 @Service
 @Transactional
 public class UserSerivceImpl implements UserService {
-
+    @Autowired
+    public UserDao  userDao;
 	@Override
 	public void save(User user) {
 		// TODO Auto-generated method stub
-		
+		userDao.save(user);
 	}
 
 	@Override
 	public void delete(User user) {
 		// TODO Auto-generated method stub
-		
+		userDao.delete(user.getUserId());
 	}
 
 	@Override
 	public void update(User user) {
 		// TODO Auto-generated method stub
-		
+		userDao.update(user);
 	}
 
 	@Override
 	public User findOne(Integer uid) {
 		// TODO Auto-generated method stub
-		return null;
+	    User user=userDao.findOne(uid);
+		return user;
 	}
 
 	@Override
 	public List<User> findAll(User user) {
 		// TODO Auto-generated method stub
-		return null;
+		List<User> list=userDao.findAll();
+		return list;
 	}
 	
 	
